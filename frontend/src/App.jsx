31ae92d437cf054a9891026a72092384838d4a74
rhkk7157 <rhkk7157@gmail.com>
2025-01-04 22:44:12 -0800

    add Category component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,25 @@
-import React from "react";
+
 import "./App.css";
+import CategoryList from "./components/CategoryList/CategoryList.jsx";
 import TodoBoard from "./components/TodoBoard";
 import { Row, Col, Container } from "react-bootstrap";
 
 const App = () => {
   return (
-    <Container>
-      <Row className="add-item-row">
-        <Col xs={12} sm={10}>
-          <input type="text" placeholder="Enter tasks" className="input-box" />
-        </Col>
-        <Col xs={12} sm={2}>
-          <button className="button-add">Add</button>
-        </Col>
-      </Row>
-      <TodoBoard />
-    </Container>
+    <>
+      <Container>
+        <Row className="add-item-row">
+          <Col xs={12} sm={10}>
+            <input type="text" placeholder="Enter tasks" className="input-box" />
+          </Col>
+          <Col xs={12} sm={2}>
+            <button className="button-add">Add</button>
+          </Col>
+        </Row>
+        <TodoBoard />
+      </Container>
+      <CategoryList />
+    </>
   );
 };
 
