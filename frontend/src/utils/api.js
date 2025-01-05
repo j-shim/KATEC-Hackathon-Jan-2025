d034123345a849e62ed34d13bd463adfd395faf6
jamesjo <jamescakr@gmail.com>
2025-01-05 15:45:01 -0700

    updated frontend + api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+  // baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
