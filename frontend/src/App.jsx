33965aa617772ba03f7dc4f4b8f59493ac7d0244
Ji Young Kim <goldie.kim89@gmail.com>
2025-01-05 18:13:37 -0800

    Stashing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+    Link
+} from "react-router-dom";
 import "./App.css";
 import TodoBoard from "./components/TodoBoard/TodoBoard";
 import { Row, Col, Container } from "react-bootstrap";
 import Login from "./components/User/Login";
 import Signup from "./components/User/Signup";
+import EditUserInfo from "./components/User/EditUserInfo";
 import api from "./utils/api";
 import DonutChart from "./components/DonutChart/DonutChart";
 
@@ -41,6 +47,33 @@ const App = () => {
 
 	const handleLoginSuccess = () => {
 		setIsLoggedIn(true);
+	};
+
+	const getCsrfToken = () => {
+		const csrfToken = document.cookie
+			.split("; ")
+			.find((row) => row.startsWith("csrftoken="));
+		return csrfToken ? csrfToken.split("=")[1] : "";
+	};
+
+	const handleLogout = () => {
+		fetch("/api/users/logout/", {
+			method: "POST",
+			credentials: "include",
+			headers: {
+				"X-CSRFToken": getCsrfToken(),
+			},
+		})
+			.then((response) => {
+				if (response.ok) {
+					setIsLoggedIn(false);
+				} else {
+					throw new Error("Logout failed.");
+				}
+			})
+			.catch((error) => {
+				console.error("Error logging out:", error);
+			});
 	};
 
 	const getTasks = async () => {
@@ -87,31 +120,50 @@ const App = () => {
 	}
 
 	return (
-		<Container className="container-box">
-			<Row>
-				<Col>
-					<div className="date-box">Jan 4th 2024</div>
-				</Col>
-			</Row>
-			<Row className="add-item-row">
-				<Col xs={12} sm={10}>
-					<input
-						type="text"
-						placeholder="Enter tasks"
-						className="input-box"
-						value={todoValue}
-						onChange={(event) => setTodoValue(event.target.value)}
-					/>
-				</Col>
-				<Col xs={12} sm={2}>
-					<button className="button-add" onClick={addTask}>
-						Add
-					</button>
-				</Col>
-			</Row>
-			<TodoBoard todoList={todoList} />
-			<DonutChart />
-		</Container>
+		<Router>
+			<Container className="container-box">
+				<Row className="top-right-corner">
+					<Col xs="auto">
+						<Link to="/edituser" id="edit-button">EDIT INFO</Link>
+							{/* id="edit-button",
+						>
+							EDIT INFO
+						</button> */}
+						<button id="signout-button" onClick={handleLogout}>
+							SIGN OUT
+						</button>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<div className="date-box">Jan 4th 2024</div>
+					</Col>
+				</Row>
+				<Row className="add-item-row">
+					<Col xs={12} sm={10}>
+						<input
+							type="text"
+							placeholder="Enter tasks"
+							className="input-box"
+							value={todoValue}
+							onChange={(event) =>
+								setTodoValue(event.target.value)
+							}
+						/>
+					</Col>
+					<Col xs={12} sm={2}>
+						<button className="button-add" onClick={addTask}>
+							Add
+						</button>
+					</Col>
+				</Row>
+				<TodoBoard todoList={todoList} />
+				<DonutChart />
+			</Container>
+			<Routes>
+				<Route path="/edituser" element={<EditUserInfo />} />
+			</Routes>
+		</Router>
 	);
 };
 
